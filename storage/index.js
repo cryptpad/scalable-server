d308b2a6eb7f3fb6f1dd28eaeaf60e7a4e3793bb
Fabrice Mouhartem <fabrice.mouhartem@xwiki.com>
2024-07-16 12:18:22 +0200

    storage: Start populating `Env`

--- storage/index.js
+++ storage/index.js
@@ -1,10 +1,28 @@
 const Store = require("./storage/file");
 
-// TODO: fill from getMetadataRaw
-let Env = {};
+let Env = {
+	metadata_cache: {},
+	channel_cache: {},
+	cache_checks: {},
+
+	batchIndexReads: BatchRead("HK_GET_INDEX"),
+	batchMetadata: BatchRead('GET_METADATA'),
+};
+
+Env.checkCache = function (channel) {
+	var f = Env.cache_checks[channel] || Util.throttle(function () {
+		delete Env.cache_checks[channel];
+		if (Env.channel_cache[channel]) { return; }
+		delete Env.metadata_cache[channel];
+	}, 30000);
+	f();
+};
 
 Store.create({
 	filePath: './data/channel',
 	archivePath: './data/archive',
 	volumeId: 'channel'
-}, function() {});
+}, function(err, _store) {
+	if (err) { console.error('Error:', err); }
+	Env.store = _store;
+});
