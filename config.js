a9ce113e3ab21fc2d1e52dfad5835f2dfcaa1467
Fabrice Mouhartem <fabrice.mouhartem@xwiki.com>
2024-07-17 09:30:58 +0200

    Update config format
    - Easier access to port and address

--- config.js
+++ config.js
@@ -3,15 +3,19 @@
 module.exports = {
     infra: {
         ws: [{
-            url: 'localhost:3010'
+            address: 'localhost',
+            port: 3010
         }],
         core: [{
-            url: 'localhost:3011'
+            address: 'localhost',
+            port: 3011
         }, {
-            url: 'localhost:3012'
+            address: 'localhost',
+            port: 3012
         }],
         storage: [{
-            url: 'localhost:3014'
+            address: 'localhost',
+            port: 3014
         }]
     }
 };
