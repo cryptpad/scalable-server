9822b790aabfca772c748a9bf42b0e6a7b25da99
Fabrice Mouhartem <fabrice.mouhartem@xwiki.com>
2024-07-19 17:48:02 +0200

    refactor: merge common functions
    WIP

--- common/interface.js
+++ common/interface.js
@@ -1,6 +1,56 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2024 XWiki CryptPad Team <contact@cryptpad.org> and contributors
 const WebSocket = require("ws");
+const Express = require("express");
+const Http = require("http");
+const Util = require("./common-util.js");
+
+const DEFAULT_QUERY_TIMEOUT = 5000;
+
+let communicationManager = function(sockets) {
+    const timeout = DEFAULT_QUERY_TIMEOUT;
+
+    let response = Util.response(function(error) {
+        console.log('Client Response Error:', error);
+    });
+
+    let sendEvent = function(dest, command, args) {
+        let msg = { CMD: command, ARGS: args, FROM: myId };
+        let wsDest = sockets[dest];
+        if (!wsDest) {
+            console.error('Server ', dest, ' unreachable');
+            return false;
+        }
+        wsDest.send(JSON.stringify(msg));
+        return true;
+    };
+
+    let sendQuery = function(dest, command, args, cb) {
+        let msg = { CMD: command, ARGS: args, FROM: myId, IDX: id };
+        let wsDest = sockets[dest];
+        if (!wsDest) {
+            console.error('Server ', dest, ' unreachable');
+            return false;
+        }
+        response.expect(String(id++), cb, timeout);
+        wsDest.send(JSON.stringify(msg));
+    };
+
+    let onMessage = function(data) {
+        let msg = {};
+        try {
+            msg = JSON.parse(data);
+        } catch (err) {
+            console.log("JSON parse error:", e)
+            return;
+        }
+
+        return msg;
+    };
+
+
+    return { sendEvent, sendQuery, onMessage };
+}
 
 /* This function initializes the different ws connections from the Ws and
     * Storage components
@@ -8,12 +58,10 @@ const WebSocket = require("ws");
     * - ../config.js
     * - whoami */
 let connect = function(config) {
-    const myId = config.id;
-
     let wsConnect = function(server) {
         let ws = WebSocket('ws://' + server.host + ':' + server.port)
             .on('error', function(err) {
-                console.error('WebSocket Connection error:', err);
+                console.error("WebSocket Connection error:", err);
             })
             .on('close', function() {
                 delete ws;
@@ -32,7 +80,7 @@ let connect = function(config) {
         /* TODO: error handling */
         if (!wsConnection) {
             // setTimeout?
-            console.log('Error while connecting with core server ', i);
+            console.log("Error while connecting with Core server ", i);
         }
     });
 
@@ -40,57 +88,49 @@ let connect = function(config) {
 
     };
 
-    let sendEvent = function(dest, command, args) {
-        let msg = { CMD: command, ARGS: args, FROM: myId };
-        let wsDest = ws[dest];
-        if (!wsDest) {
-            console.error('Server ', dest, ' unreachable');
-            return false;
-        }
-        wsDest.send(JSON.stringify(msg));
-        return true;
-    };
+    let manager = communicationManager(ws);
 
-    let sendQuery = function(dest, command, args, cb) {
-        let msg = { CMD: command, ARGS: args, FROM: myId };
-        let wsDest = ws[dest];
-        if (!wsDest) {
-            console.error('Server ', dest, ' unreachable');
-            return false;
-        }
-        wsDest.send(JSON.stringify(msg));
-        // TODO: handle callback
+    return {
+        sendEvent: manager.sendEvent,
+        sendQuery: manager.sendQuery,
+        onMessage: manager.onMessage,
+        disconnect
     };
-
-    let onMessage = function(data) {
-        let msg = {};
-        try {
-            msg = JSON.parse(data);
-        } catch (err) {
-            console.log('JSON parse error:', e)
-            return;
-        }
-        return msg;
-    };
-
-    return { sendEvent, sendQuery, onMessage, disconnect };
 };
 
 /* This function initializes the different ws servers on the Core components */
 let init = function(config) {
+    let ws = [];
+
+    let wsCreate = function(server, i) {
+        let app = Express();
+        let httpServer = Http.createServer(app);
+        httpServer.listen(server.host, server.port, function() {
+            ws[i] = new WebSocket.Server({ server: httpServer });
+        });
+    };
+
+    config.infra.core.forEach(wsCreate);
+
+    ws.forEach((wsConnection, i) => {
+        /* TODO: error handling */
+        if (!wsConnection) {
+            // setTimeout?
+            console.log("Error while creating the Core server ", i);
+        }
+    });
+
     let disconnect = function() {
     };
 
-    let sendEvent = function(dest, command, args) {
-    };
+    let manager = communicationManager(ws);
 
-    let sendQuery = function(dest, command, args, cb) {
+    return {
+        sendEvent: manager.sendEvent,
+        sendQuery: manager.sendQuery,
+        onMessage: manager.onMessage,
+        disconnect
     };
-
-    let onMessage = function(data) {
-    };
-
-    return { sendEvent, sendQuery, onMessage, disconnect };
 };
 
 module.exports = { connect, init };
