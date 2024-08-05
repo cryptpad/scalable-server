661204c2e627fb84359c7900dd064595cdf21d28
Fabrice Mouhartem <fabrice.mouhartem@xwiki.com>
2024-08-05 10:38:54 +0200

    refactor(tests): change naming convention + draft request tests

---
+++ tests/requests.test.js
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-FileCopyrightText: 2024 XWiki CryptPad Team <contact@cryptpad.org> and contributors
+
+/* This is a test of real-world requests, namely `GET_HISTORY` and
+ * `GET_METADATA`.
+ * The goal of this testsuite is to:
+ * - create a new datastore
+ * - populate it with some pads
+ * - make metadata queries
+ * - fill the pad with some patches
+ * - make metadata and history queries
+ * - check consistency
+ **/
