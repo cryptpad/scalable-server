84ab4478631b03f0313f7972d27588c5c67e7a3e
Fabrice Mouhartem <fabrice.mouhartem@xwiki.com>
2024-08-06 17:20:12 +0200

    refactor(crypto): update the API to fit cryptpad
    - from https://github.com/cryptpad/cryptpad-sodium-plugin

--- core/crypto.js
+++ core/crypto.js
@@ -1,23 +1,22 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2024 XWiki CryptPad Team <contact@cryptpad.org> and contributors
-const NaCl = require("tweetnacl/nacl-fast");
-const SodiumNative = require("sodium-native");
 
 module.exports = cryptoLib => {
     let exports = {};
     exports.decodeBase64 =  msg => Buffer.from(msg, 'base64');
     switch (cryptoLib) {
-        case 'tweetnacl':
-            exports.sigVerify = NaCl.sign.open; // (message, key) -> bool
-            break;
         case 'sodiumnative':
+            const SodiumNative = require("sodium-native");
             exports.sigVerify = (signedMessage, validateKey) => {
                 let msg = signedMessage.subarray(64);
                 return SodiumNative.crypto_sign_open(msg, signedMessage, validateKey);
-            }
+            };
+            exports.detachedVerify = (signedBuffer, signatureBuffer, validateKey) => SodiumNative.crypto_sign_verify_detached(signatureBuffer, signedBuffer, validateKey);
             break;
-        default:
+        default: // tweetNaCl
+            const NaCl = require("tweetnacl/nacl-fast");
             exports.sigVerify = NaCl.sign.open;
+            exports.detachedVerify = NaCl.sign.detached.verify;
             break;
     }
     return exports;
