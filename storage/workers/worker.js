fb0011425a4460901686d26ef303d65067e0a816
Fabrice Mouhartem <fabrice.mouhartem@xwiki.com>
2024-10-17 17:17:55 +0200

    feat(db-worker): Add commands management (wip)
    - functions are not yet linked

--- storage/workers/worker.js
+++ storage/workers/worker.js
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 const Util = require("../common-util.js");
+const ChannelManager = require("../channel_manager.js");
 
 const Env = {};
 
@@ -10,4 +11,47 @@ const init = (conf, _cb) => {
     if (!conf) {
         return void cb('E_INVALID_CONFIG');
     };
+
+    Env.paths = {
+        baseDir: conf.baseDir,
+    };
+
+    Env.CM = ChannelManager.create(Env, Env.paths.baseDir);
 };
+
+const COMMANDS = {
+    COMPUTE_INDEX: computeIndex,
+    COMPUTE_METADATA: computeMetadata,
+};
+
+process.on('message', function (data) {
+    if (!data || !data.txid || !data.pid) {
+        return void process.send({
+            error:'E_INVAL',
+            data: data,
+        });
+    }
+
+    const cb = function (err, value) {
+        process.send({
+            error: Util.serializeError(err),
+            txid: data.txid,
+            pid: data.pid,
+            value: value,
+        });
+    };
+
+    if (!ready) {
+        return void init(data.config, function (err) {
+            if (err) { return void cb(Util.serializeError(err)); }
+            ready = true;
+            cb();
+        });
+    }
+
+    const command = COMMANDS[data.command];
+    if (typeof(command) !== 'function') {
+        return void cb("E_BAD_COMMAND");
+    }
+    command(data, cb);
+});
