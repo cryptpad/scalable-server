bcbe00b88ebcd9d4b8a6021ce1b1375df0c3bd7e
Fabrice Mouhartem <fabrice.mouhartem@xwiki.com>
2024-10-23 11:56:23 +0200

    dev: add .editorfile and linter rules

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            semi: "error",
+            indent: ["error", 4],
+            "no-useless-escape": "off",
+            "no-unused-vars": "warn",
+        }
+    },
+];
