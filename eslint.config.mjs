7b04c00fef829bff074a1bf20bc5d7cb4949e4bd
yflory <yann.flory@xwiki.com>
2026-02-20 11:46:12 +0100

    chore(lint): Add eslint rules for browser code in plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,32 +2,47 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 
-const ignores = ["scripts/**", "build/**", "common/common-util.js"];
-export default defineConfig([
-  {
+const rules = {
+    indent: ["off", 4],
+    "linebreak-style": ["off", "unix"],
+    quotes: ["off", "single"],
+    semi: ["error", "always"],
+    eqeqeq: ["error", "always"],
+    "no-irregular-whitespace": ["off"],
+    "no-self-assign": ["off"],
+    "no-empty": ["off"],
+    "no-useless-escape": ["off"],
+    "no-extra-boolean-cast": ["off"],
+    "no-prototype-builtins": ["error"],
+    "no-use-before-define": ["error"],
+    "no-undef": ["error"],
+    "no-unused-vars": [
+        "error",
+        {
+            caughtErrors: "none"
+        }
+    ]
+};
+
+export default defineConfig([{
     files: ["**/*.{js,mjs,cjs}"],
-    ignores,
+    ignores: [
+        "scripts/**", "build/**",
+        "plugins/*/client/**", "common/common-util.js"
+    ],
     languageOptions: { globals: globals.node },
-    rules: {
-        indent: ["off", 4],
-        "linebreak-style": ["off", "unix"],
-        quotes: ["off", "single"],
-        semi: ["error", "always"],
-        eqeqeq: ["error", "always"],
-        "no-irregular-whitespace": ["off"],
-        "no-self-assign": ["off"],
-        "no-empty": ["off"],
-        "no-useless-escape": ["off"],
-        "no-extra-boolean-cast": ["off"],
-        "no-prototype-builtins": ["error"],
-        "no-use-before-define": ["error"],
-        "no-undef": ["error"],
-        "no-unused-vars": [
-            "error",
-            {
-                caughtErrors: "none"
-            }
-        ]
-    }
-  }
-]);
+    rules
+}, {
+    files: ["plugins/*/client/**.{js,mjs,cjs}"],
+    ignores: ["common/common-util.js"],
+    languageOptions: {
+        globals: { ...globals.browser, ...globals.amd }
+    },
+    rules
+}, {
+    files: ["common/common-util.js"],
+    languageOptions: {
+        globals: { ...globals.node, ...globals.browser, ...globals.amd }
+    },
+    rules
+}]);
